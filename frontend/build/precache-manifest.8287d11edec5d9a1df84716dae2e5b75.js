daeca5a87ba1de4a5ed0bb2de9622ca0a0fd1d1b
solllull <llullmariasol@gmail.com>
2023-07-20 17:59:44 -0300

    Add frontend React folder

---
+++ frontend/build/precache-manifest.8287d11edec5d9a1df84716dae2e5b75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "153b766d39b3943ad86c",
+    "url": "/static/css/main.62e5a194.chunk.css"
+  },
+  {
+    "revision": "153b766d39b3943ad86c",
+    "url": "/static/js/main.ed89979e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58bcf36d64592815c00f",
+    "url": "/static/js/2.d7be0b7f.chunk.js"
+  },
+  {
+    "revision": "f6a9792d28b2fc5c6285a58ede27c63e",
+    "url": "/static/media/add.f6a9792d.svg"
+  },
+  {
+    "revision": "b3c2e7e4179fcf28e68c0053e89bd1e8",
+    "url": "/static/media/arrow-left.b3c2e7e4.svg"
+  },
+  {
+    "revision": "202b7acb7aaa919f40660e8b32d462c1",
+    "url": "/index.html"
+  }
+];
---
+++ frontend/src/pages/NotePage.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+
+const NotePage = () => {
+  let { id } = useParams();
+  const navigate = useNavigate();
+  let [note, setNote] = useState({});
+
+  useEffect(() => {
+    getNote(id);
+  }, [id]);
+
+  let getNote = async () => {
+    if (id === "new") {
+      return;
+    }
+    let response = await fetch(`/api/notes/${id}/`);
+    let data = await response.json();
+    setNote(data);
+  };
+
+  let updateNote = async () => {
+    await fetch(`/api/notes/${id}/update/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let deleteNote = async () => {
+    await fetch(`/api/notes/${id}/delete/`, {
+      method: "DELETE",
+    });
+    navigate("/");
+  };
+
+  let createNote = async () => {
+    await fetch(`/api/notes/create/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let handleSubmit = () => {
+    if (id !== "new" && note.body === '') {
+      deleteNote();
+    } else if (id !== "new") {
+      updateNote();
+    } else if (id === "new" && note !== null) {
+      createNote();
+    }
+
+    navigate("/");
+  };
+
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <ArrowLeft onClick={handleSubmit} />
+          </Link>
+        </h3>
+
+        {id !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
+      </div>
+      <textarea
+        onChange={(e) => setNote({ ...note, body: e.target.value })}
+        value={note.body}
+      ></textarea>
+    </div>
+  );
+};
+
+export default NotePage;
